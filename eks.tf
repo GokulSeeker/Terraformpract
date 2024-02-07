97e344fb69ae486c8cb1de13997c2ff90d4d7cdf
Gokul K <gokulaws1311@gmail.com>
2024-02-07 18:10:28 +0530

    just key

--- eks.tf
+++ eks.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAS3D7OVI4K7TBNQSZ"
+  access_key = "AKIAS3D7OVI4K7TBNQSZ
   secret_key = "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"
-}
+} add
 
 terraform {
   cloud {
